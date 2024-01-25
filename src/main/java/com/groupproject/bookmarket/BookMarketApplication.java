b2d3b91d374eb1d209e9070ad087c303b19153db
Rocky-cyberclop <rockyoperation@gmail.com>
2024-01-25 16:13:32 +0700

    Set up base

---
+++ src/main/java/com/groupproject/bookmarket/BookMarketApplication.java
@@ -0,0 +1,14 @@
+package com.groupproject.bookmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMarketApplication.class, args);
+		System.out.println("Done!");
+	}
+
+}
